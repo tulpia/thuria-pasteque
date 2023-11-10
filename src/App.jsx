96e127588e3653740ccb0c61d43282aa17ef6551
alec <a.teihotaata@thuria.com>
2023-11-10 13:59:55 +0100

    [feature] Ajout de la collision

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import Matter from "matter-js";
+import { MatterCollisionEvents } from "matter-collision-events";
 import { useEffect, useState, useRef } from "react";
 import "./App.css";
 
@@ -17,35 +18,86 @@ import red from './assets/red.png';
 import black from './assets/black.png';
 import orange from './assets/orange.png';
 
+Matter.use(MatterCollisionEvents);
+
 function App() {
   const config = {
     width: 500,
     height: 600,
   };
   const [engine, setEngine] = useState();
+  const floor = useRef(null);
+  const wallLeft = useRef(null);
+  const wallRight = useRef(null);
   const [nextCircle, setNextCircle] = useState(circles[0]);
   const boxRef = useRef(null);
   const canvasRef = useRef(null);
 
   const handleAddCircle = (e) => {
     const mousePos = getMousePos(canvasRef.current, e);
-    const ball = Matter.Bodies.circle(mousePos.x, 0, nextCircle.radius, {
-      restitution: nextCircle.restitution,
+    
+    createCircle(mousePos.x, 0, nextCircle, true);
+  };
+
+  const createCircle = (x, y, selectedCircle, isCreation = false) => {
+    const ball = Matter.Bodies.circle(x, y, selectedCircle.radius, {
+      restitution: selectedCircle.restitution,
 
       render: {
         sprite: {
-          texture: eval(nextCircle.fill)
+          texture: eval(selectedCircle.fill)
         }
       },
     });
 
     Matter.World.add(engine.world, [ball]);
 
+    // On figure out quel est l'élément àjouter après que cet élément soit fusionné
+    let circleIndex = null;
+
+    circles.forEach((circle, index) => {
+      if (circleIndex === null && circle.radius === ball.circleRadius) {
+        circleIndex = index + 1;
+
+        if (circleIndex > circles.length - 1) {
+          circleIndex = null
+        }
+      }
+    });
+
+    // Ajout de l'event sur l'objet
+    if (circleIndex !== null) {
+      ball.onCollide(pair => {
+        if (pair.bodyA.id !== floor.current.id) {
+          let ballCollided = null;
+  
+          if (pair.bodyA.id !== ball.id) {
+            ballCollided = pair.bodyA;
+          }
+  
+          if (pair.bodyB.id !== ball.id) {
+            ballCollided = pair.bodyB;
+          }
+  
+          if (ballCollided) {
+            if (ballCollided.circleRadius === ball.circleRadius && ballCollided.position.y > ball.position.y) {
+              Matter.World.remove(engine.world, ball);
+              Matter.World.remove(engine.world, ballCollided);
+  
+              createCircle(ball.position.x, ball.position.y, circles[circleIndex], false);
+            }
+          }
+        }
+      });
+    }
+
     // Changement du prochain cercle
-    const randomInteger =
-      Math.floor(Math.random() * (circles.length - 1 - 0 + 1)) + 0;
-    setNextCircle(circles[randomInteger]);
-  };
+    if (isCreation) {
+      const randomInteger =
+        Math.floor(Math.random() * (circles.length - 2 - 0 + 1)) + 0;
+      setNextCircle(circles[randomInteger]);
+    }
+  }
 
   const getMousePos = (canvas, evt) => {
     const rect = canvas.getBoundingClientRect();
@@ -72,7 +124,7 @@ function App() {
 
     setEngine(engine);
 
-    const floor = Matter.Bodies.rectangle(
+    floor.current = Matter.Bodies.rectangle(
       config.width / 2,
       config.height,
       config.width,
@@ -84,7 +136,8 @@ function App() {
         },
       }
     );
-    const wallLeft = Matter.Bodies.rectangle(
+    
+    wallLeft.current = Matter.Bodies.rectangle(
       0,
       config.height / 2,
       20,
@@ -96,7 +149,7 @@ function App() {
         },
       }
     );
-    const wallRight = Matter.Bodies.rectangle(
+    wallRight.current = Matter.Bodies.rectangle(
       config.width,
       config.height / 2,
       20,
@@ -109,7 +162,7 @@ function App() {
       }
     );
 
-    Matter.World.add(engine.world, [floor, wallLeft, wallRight]);
+    Matter.World.add(engine.world, [floor.current, wallLeft.current, wallRight.current]);
 
     Matter.Runner.run(engine);
     Matter.Render.run(render);
--- src/utils/circles.js
+++ src/utils/circles.js
@@ -2,32 +2,32 @@ const circles = [
   {
     radius: 10,
     fill: "yellow",
-    restitution: 0.3,
+    restitution: 0.55,
   },
   {
     radius: 20,
     fill: "green",
-    restitution: 0.25,
+    restitution: 0.5,
   },
   {
     radius: 30,
     fill: "blue",
-    restitution: 0.2,
+    restitution: 0.45,
   },
   {
     radius: 40,
     fill: "red",
-    restitution: 0.15,
+    restitution: 0.4,
   },
   {
     radius: 50,
     fill: "black",
-    restitution: 0.1,
+    restitution: 0.35,
   },
   {
     radius: 60,
     fill: "orange",
-    restitution: 0.05,
+    restitution: 0.3,
   },
 ];
 
